46ce39c39c6d574558811c4d699177080e98c373
unknown <rajkumar.d408@gmail.com>
2022-12-01 18:56:31 +0530

    origin

---
+++ src/auth/token.service.ts
@@ -0,0 +1,14 @@
+import { ITokenService } from "./ITokenService";
+//@ts-ignore
+// eslint-disable-next-line import/no-unresolved
+import { TokenServiceBase } from "./base/token.service.base";
+//@ts-ignore
+export class TokenService extends TokenServiceBase implements ITokenService {
+    /**
+ * @param bearer
+ * @returns the username from a jwt token
+ */
+decodeToken(bearer: string): string {
+    return this.jwtService.verify(bearer).username;
+  }
+}
